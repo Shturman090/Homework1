c4261d45ecc1aa237b4de89b636c657c1bc66417
Shturman090 <s73mavlutov@gmail.com>
2024-11-19 22:47:32 +1000

    Add files via upload

---
+++ ПЗ_ПЕРЕМЕННЫЕ.py
@@ -0,0 +1,11 @@
+completed_tasks = 12
+
+total_time = 1.5
+
+path = "Python"
+
+average_time = (total_time /completed_tasks)
+
+
+
+print ('Курс:', path + "," , 'всего задач:', str(completed_tasks) + ",", 'затрачено часов:', str(total_time) + ",", 'среднее время выполнения:', average_time, 'часа.')
