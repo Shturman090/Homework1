a53e2e1bdd4179117595404704cf16ceb1c5a520
Радмир <s73mavlutov@gmail.com>
2024-11-24 21:42:10 +1000

    Add files via upload

---
+++ ZADANIE- 'SREDNIY BALL'.py
@@ -0,0 +1,12 @@
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted_students = sorted(students)
+
+x=[]
+for i in grades:
+    x.append(sum(i) / len(i))
+
+list_students = dict(zip(sorted_students, x))
+print(list_students)
+
