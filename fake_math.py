aac5bfd45a550fc31f1005b866104675a2b4e261
Радмир <s73mavlutov@gmail.com>
2024-12-18 23:12:00 +1000

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,13 @@
+# fake_math
+def divide (first, second):
+        if second == 0:
+            return 'Ошибка'
+        else:
+            division = first / second
+            return division
+
+        divide()
+
+
+# result1 = divide(4,0)
+# print(result1)
---
+++ true_math.py
@@ -0,0 +1,15 @@
+
+# true_math
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        division = first / second
+        return division
+
+    divide()
+
+# result1 = divide(4,0)
+# print(result1)
